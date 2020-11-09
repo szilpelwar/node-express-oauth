d402a87ee88fb95b2fa0d8ddf5c0ad431bbd8d55
Swapnil Zilpelwar <s.zilpelwar@gmail.com>
2020-11-09 15:14:31 -0500

    Update authorization-server.js

--- authorization-server.js
+++ authorization-server.js
@@ -8,6 +8,7 @@ const {
 	decodeAuthCredentials,
 	timeout,
 } = require("./utils")
+const { decode } = require("querystring")
 
 const config = {
 	port: 9001,
@@ -50,6 +51,105 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Start
+app.get("/authorize", (req,res) => {
+ const clientId = req.query.client_id;
+ const client = clients[clientId];
+if (!client) {
+	res.status(401).send(" Error : Client not Authorized");
+	return;
+}
+if(typeof req.query.scope !== "string" ||
+   !containsAll(client.scopes, req.query.scope.split(" "))){
+	res.status(401).send("Error : Invalid scopes requested");
+	return;
+   }
+
+   const requestId = randomString();
+   requests[requestId] = req.query;
+
+   res.render("login",{
+	   client,
+	   scope: req.query.scope,
+	   requestId
+   })
+
+})
+
+
+app.post("/approve", (req,res) =>{
+	const {userName,password,requestId} = req.body
+
+	if (!userName || users[userName] != password){
+		res.status(401).send(" Error : User not Authorized");
+	return;
+	}
+
+	const clientReq = requests[requestId];
+	delete requests[requestId]
+
+	if(!clientReq){
+		res.status(401).send(" Error : Invalid User Request");
+	return;
+	}
+	const code = randomString();
+
+	authorizationCodes[code] = {clientReq,userName}
+
+	const redirectUri = url.parse(clientReq.redirect_uri)
+	redirectUri.query = {
+		code,
+		state: clientReq.state,
+	}
+
+	res.redirect(url.format(redirectUri))
+})
+
+app.post("/token", (req,res)=>{
+	let authCredentials = req.headers.authorization
+
+	if(!authCredentials){
+		res.status(401).send(" Error : Not Authorized");
+	return;
+	}
+
+	const {clientId, clientSecret} = decodeAuthCredentials(authCredentials);
+
+	const client = clients[clientId]
+	if(!client || client.clientSecret !== clientSecret){
+		res.status(401).send(" Error : Client Not Authorized");
+	return;
+	}
+
+	const code = req.body.code
+	if(!code || !authorizationCodes[code]){
+		res.status(401).send(" Error : Invalid Code");
+	return;
+	}
+
+	const {clientReq,userName} = authorizationCodes[code]
+	delete authorizationCodes[code]
+
+	const token = jwt.sign(
+		{
+			userName,
+			scope: clientReq.scope
+		},
+		config.privateKey,
+		{
+			algorithm: "RS256",
+			expiresIn: 300,
+			issuer: "http://localhost" + config.port,
+		}
+	)
+
+	res.json({
+		access_token : token,
+		token_type: "Bearer",
+		scope: clientReq.scope,
+	})
+})
+//end
 /*
 Your code here
 */
