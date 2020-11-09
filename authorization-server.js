f2369dc6b4a306a602f943f21b2d242ebf496130
Swapnil Zilpelwar <s.zilpelwar@gmail.com>
2020-11-09 15:53:34 -0500

    Update authorization-server.js

--- authorization-server.js
+++ authorization-server.js
@@ -9,7 +9,6 @@ const {
 	decodeAuthCredentials,
 	timeout,
 } = require("./utils")
-const { decode } = require("querystring")
 
 const config = {
 	port: 9001,
@@ -52,31 +51,28 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// Start
-app.get("/authorize", (req,res) => {
- const clientId = req.query.client_id;
- const client = clients[clientId];
-if (!client) {
-	res.status(401).send(" Error : Client not Authorized");
-	return;
-}
-if(typeof req.query.scope !== "string" ||
-   !containsAll(client.scopes, req.query.scope.split(" "))){
-	res.status(401).send("Error : Invalid scopes requested");
-	return;
-   }
-
-   const requestId = randomString();
-   requests[requestId] = req.query;
-
-   res.render("login",{
-	   client,
-	   scope: req.query.scope,
-	   requestId
-   })
-
+app.get("/authorize", (req, res) => {
+	const clientId = req.query.client_id
+	const client = clients[clientId]
+	if (!client) {
+		res.status(401).send("Error: client not authorized")
+		return
+	}
+	if (
+		typeof req.query.scope !== "string" ||
+		!containsAll(client.scopes, req.query.scope.split(" "))
+	) {
+		res.status(401).send("Error: invalid scopes requested")
+		return
+	}
+	const requestId = randomString()
+	requests[requestId] = req.query
+	res.render("login", {
+		client,
+		scope: req.query.scope,
+		requestId,
+	})
 })
-
 
 app.post("/approve", (req, res) => {
 	const { userName, password, requestId } = req.body
@@ -100,59 +96,47 @@ app.post("/approve", (req, res) => {
 	res.redirect(url.format(redirectUri))
 })
 
-app.post("/token", (req,res)=>{
+app.post("/token", (req, res) => {
 	let authCredentials = req.headers.authorization
-
-	if(!authCredentials){
-		res.status(401).send(" Error : Not Authorized")
-	return;
+	if (!authCredentials) {
+		res.status(401).send("Error: not authorized")
+		return
 	}
-
-	const {clientId, clientSecret} = decodeAuthCredentials(authCredentials);
-
+	const { clientId, clientSecret } = decodeAuthCredentials(authCredentials)
 	const client = clients[clientId]
-	if(!client || client.clientSecret !== clientSecret){
-		res.status(401).send(" Error : Client Not Authorized")
-	return;
+	if (!client || client.clientSecret !== clientSecret) {
+		res.status(401).send("Error: client not authorized")
+		return
 	}
-
 	const code = req.body.code
-	if(!code || !authorizationCodes[code]){
-		res.status(401).send(" Error : Invalid Code")
-	return;
+	if (!code || !authorizationCodes[code]) {
+		res.status(401).send("Error: invalid code")
+		return
 	}
-
-	const {clientReq,userName} = authorizationCodes[code]
+	const { clientReq, userName } = authorizationCodes[code]
 	delete authorizationCodes[code]
-
 	const token = jwt.sign(
 		{
 			userName,
-			scope: clientReq.scope
+			scope: clientReq.scope,
 		},
 		config.privateKey,
 		{
 			algorithm: "RS256",
 			expiresIn: 300,
-			issuer: "http://localhost" + config.port,
+			issuer: "http://localhost:" + config.port,
 		}
 	)
-
 	res.json({
-		access_token : token,
+		access_token: token,
 		token_type: "Bearer",
 		scope: clientReq.scope,
 	})
 })
-//end
-/*
-Your code here
-*/
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
 	var port = server.address().port
-	console.log("OAuth Server is listening at http://%s:%s", host, port)
 })
 
 // for testing purposes
